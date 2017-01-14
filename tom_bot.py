709462f6b837a7f8fdd4eada2a81b0b1ce1bda62
Monika Hoex <monikahoex@gmail.com>
2017-01-13 23:13:20 -0800

    Change to Markovbot Library

--- tom_bot.py
+++ tom_bot.py
@@ -1,101 +1,27 @@
-import os
+# Import Libraries
+import markovify
+import tweepy
 import time
+import os
 
-from markovbot import MarkovBot
+# Import auth keys
+from auth import *
 
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
 
-# # # # #
-# INITIALISE
+# Generate Tweet
+def generate_tweet():
 
-# Initialise a MarkovBot instance
-tweetbot = MarkovBot()
+    with open(os.path.dirname(__file__) + "lyrics.txt") as f:
+        text = f.read()
 
-# Get the current directory's path
-dirname = os.path.dirname(os.path.abspath(__file__))
-# Construct the path to the lyrics
-lyrics = os.path.join(dirname, u'lyrics.txt')
-# Make your bot read the book!
-tweetbot.read(lyrics)
+    text_model = markovify.NewlineText(text)
 
+    output_text = text_model.make_short_sentence(121) + " #TomTale #TomWaits"
 
-# # # # #
-# TEXT GENERATION
+    return output_text
 
-# Generate text by using the generate_text method:
-# 	The first argument is the length of your text, in number of words
-# 	The 'seedword' argument allows you to feed the bot some words that it
-# 	should attempt to use to start its text. It's nothing fancy: the bot will
-# 	simply try the first, and move on to the next if he can't find something
-# 	that works.
-tweet = tweetbot.generate_text(25, seedword=[u'dream', u'Jesus', u'moon', u'girl', u'Whiskey', u'letter'])
-
-# Print your text to the console
-print(u'\ntombot says: "%s"' % (tweet))
-
-
-# # # # #
-# TWITTER
-
-# The MarkovBot uses @sixohsix' Python Twitter Tools, which is a Python wrapper
-# for the Twitter API. Find it on GitHub: https://github.com/sixohsix/twitter
-
-# ALL YOUR SECRET STUFF!
-# Make sure to replace the ''s below with your own values, or try to find
-# a more secure way of dealing with your keys and access tokens. Be warned
-# that it is NOT SAFE to put your keys and tokens in a plain-text script!
-
-# Consumer Key (API Key)
-cons_key = 'tvhrprOnoc15QIsUJQOx80MtQ'
-# Consumer Secret (API Secret)
-cons_secret = 'QezDqVGOtrJ7wtZVD9TObtwyiE0S9ANKcpwasoqZ640m355HuM'
-# Access Token
-access_token = '816386163753631744-6FubxJjuht642UJzB2vJrQQUOZEPixh'
-# Access Token Secret
-access_token_secret = 'dENleCfGu4lA64ToewVr68AAwRTpJb7vB0mO1QqbGryGt'
-
-# Log in to Twitter
-tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
-
-# The target string is what the bot will reply to on Twitter. To learn more,
-# read: https://dev.twitter.com/streaming/overview/request-parameters#track
-targetstring = 'TalkToTom'
-# Keywords are words the bot will look for in tweets it'll reply to, and it
-# will attempt to use them as seeds for the reply
-keywords = [u'dream', u'Jesus', u'moon', u'girl', u'whiskey', u'letter']
-# The prefix will be added to the start of all outgoing tweets.
-prefix = None
-# The suffix will be added to the end of all outgoing tweets.
-suffix = '#TomTalks'
-# The maxconvdepth is the maximum depth of the conversation that the bot will
-# still reply to. This is relevant if you want to reply to all tweets directed
-# at a certain user. You don't want to keep replying in the same conversation,
-# because that would be very annoying. Be responsible, and allow your bot only
-# a shallow conversation depth. For example, a value of 2 will allow the bot
-# to only reply in conversations where there are two or less replies to the
-# original tweet.
-maxconvdepth = 3
-
-# Start auto-responding to tweets by calling twitter_autoreply_start
-# This function operates in a Thread in the background, so your code will not
-# block by calling it.
-tweetbot.twitter_autoreply_start(targetstring, keywords=keywords, prefix=prefix, suffix=suffix, maxconvdepth=maxconvdepth)
- 
-# Start periodically tweeting. This will post a tweet every X days, hours, and
-# minutes. (You're free to choose your own interval, but please don't use it to
-# spam other people. Nobody likes spammers and trolls.)
-# This function operates in a Thread in the background, so your code will not
-# block by calling it.
-tweetbot.twitter_tweeting_start(days=0, hours=19, minutes=30, keywords=None, prefix=None, suffix='#TomTales #TomWaits')
-
-# DO SOMETHING HERE TO ALLOW YOUR BOT TO BE ACTIVE IN THE BACKGROUND
-# You could, for example, wait for a week:
-secsinweek = 7 * 24 * 60 * 60
-time.sleep(secsinweek)
- 
-# Use the following to stop auto-responding
-# (Don't do this directly after starting it, or your bot will do nothing!)
-# tweetbot.twitter_autoreply_stop()
-
-# Use the following to stop periodically tweeting
-# (Don't do this directly after starting it, or your bot will do nothing!)
-# tweetbot.twitter_tweeting_stop()
+# Post the tweet to Twitter
+api.update_status(status=generate_tweet())
